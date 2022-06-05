16d6db19f57fc5d50c531d260f8f6e3962594555
Prince <103827570+onukwilip@users.noreply.github.com>
2022-06-05 23:51:11 +0100

    Initial

---
+++ AllMediaArray.js
@@ -0,0 +1,23 @@
+﻿var _array = [1, 7, 2, 5];
+
+function myarrayinput()
+{
+    _array.sort();
+    var all = document.getElementById("arrayinput");
+    _array.push(all.value);
+    var ok = document.getElementById("lastno").value;
+    
+    //document.getElementById("lastno").value = all.value;
+    document.getElementById("final").innerHTML = _array;
+    document.getElementById("last").innerHTML = _array[_array.length - 1];
+    //document.getElementById("last").innerHTML = document.getElementById("lastno").value;
+    
+    //alert(document.getElementById("lastno").value);
+}
+
+function display()
+{
+    _array.sort();
+    document.getElementById("final").innerHTML = _array;
+    document.getElementById("last").value = document.getElementById("lastno").value;
+}
